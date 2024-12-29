dc6b0e99b5c114cf4af0144cadd5434c30007abb
Haeryz <haeriz42069@gmail.com>
2024-12-29 21:27:26 +0700

    Add Navbar component and implement routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
-import { Button, HStack } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
+import Navbar from './components/main/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Account from './pages/Account';
+import Wishlist from './pages/Wishlist';
+import Items from './pages/Items';
+import { useColorModeValue } from './components/ui/color-mode';
 
 function App() {
   return (
-    <HStack>
-      <Button>
-        Ohio
-      </Button>
-      <Button>
-        mewing
-      </Button>
-    </HStack>
-  )
+    <Router>
+      <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/items" element={<Items />} />
+        </Routes>
+      </Box>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/main/Navbar.jsx
@@ -0,0 +1,63 @@
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { useColorMode } from "../ui/color-mode";
+import { Link } from "react-router";
+import { FcDataBackup } from "react-icons/fc";
+import { RiAccountPinBoxLine } from "react-icons/ri";
+import { FaRegHeart } from "react-icons/fa";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { IoMoon } from "react-icons/io5";
+import { IoSunnyOutline } from "react-icons/io5";
+
+function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", md: "row" }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500, purple.600)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>
+            {" "}
+            <FcDataBackup /> RovenShop
+          </Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/account"}>
+            <Button>
+              <RiAccountPinBoxLine fontSize={20} />
+            </Button>
+          </Link>
+
+          <Link to={"/wishlist"}>
+            <Button>
+              <FaRegHeart fontSize={20} />
+            </Button>
+          </Link>
+
+          <Link to={"/items"}>
+            <Button>
+              <HiOutlineShoppingBag fontSize={20} />
+            </Button>
+          </Link>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <IoSunnyOutline />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+}
+
+export default Navbar;
