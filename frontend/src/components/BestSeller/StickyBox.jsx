b9ec37c7519654bc030ab8f305008b2b72937b0c
frhnaml <achmalfaran@gmail.com>
2024-12-30 16:57:20 +0700

    BestSeller part2

---
+++ frontend/src/components/BestSeller/StickyBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';  
+import { Box, Flex, Text } from '@chakra-ui/react';   
+
+function StickyBox() {  
+  return (  
+    <Box  
+      position="sticky"  
+      top="0" // Tetap di bagian atas  
+      bg="white" // Latar belakang putih  
+      p={8} // Padding  
+    //   boxShadow="md" // Bayangan untuk efek kedalaman  
+      border="1px solid" // Border dengan ketebalan 1px  
+      borderColor="gray.300" // Warna border abu-abu  
+      zIndex="1000"  
+    >  
+      <Flex justify="center" align="center">  
+        {/* Konten StickyBox */}
+        <Text fontSize="xl" fontWeight="bold" color="gray.600" textAlign="center">
+          Parfume with a choice of luxury only
+        </Text>
+      </Flex>  
+    </Box>  
+  );  
+}  
+
+export default StickyBox;
--- frontend/src/pages/BestSeller.jsx
+++ frontend/src/pages/BestSeller.jsx
@@ -1,36 +1,25 @@
-import { Bleed, Box, Heading, Stack, Text, Container } from '@chakra-ui/react';  
 import React from 'react';  
-import bgBestSellerImage from '../assets/bgBestSeller.jpg'; // Import gambar dengan path yang benar  
+import FrstBox from '../components/BestSeller/FrstBox.jsx'; // Pastikan path ini sesuai  
+import ScndBox from '../components/BestSeller/ScndBox.jsx';  
+import { Container, Flex, Box, VStack } from '@chakra-ui/react';  
 
 
 function BestSeller() {  
-  return (  
-    <Box  
-      bgImage={`url(${bgBestSellerImage})`} // Menggunakan variabel image  
-      bgSize="cover"  
-      bgPosition="center"  
-      h="100vh"  
-      color="white"  
-      display="flex"  
-      alignItems="center"  
-      justifyContent="center"  
-    >  
-      <Container textAlign="center">  
-        <Text fontSize="4xl" fontWeight="bold">  
-          Best Sellers  
-        </Text>  
-        <Text fontSize="lg" mt={4}>  
-          Don't know where to start?  
-        </Text>  
-        <Text fontSize="lg" mt={2}>  
-          Here's our full line up of our best sellers  
-        </Text>  
-      </Container>  
-      
-        
-
-    </Box>  
-  );  
+    return (  
+        <Container maxW="container.xl" p={0}>  
+            <VStack spacing={8} align="stretch"> {/* Menambahkan spacing antar elemen */}  
+                <Box>  
+                    <FrstBox />  
+                </Box>  
+                {/* <Box>  
+                    <StickyBox />  
+                </Box>   */}
+                <Box>  
+                    <ScndBox />  
+                </Box>  
+            </VStack>  
+        </Container>  
+    );  
 }  
 
 export default BestSeller;
