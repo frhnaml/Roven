3c8f00c58b7ecd0259d18160895541b41485b671
andifathir <andi.fathirr@gmail.com>
2024-12-30 13:05:55 +0700

    added 4 table to the database

---
+++ roven-backend/app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $primaryKey = 'order_id';
+    
+    protected $fillable = [
+        'user_id',
+        'total_amount',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ roven-backend/app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $primaryKey = 'product_id';
+    
+    protected $fillable = [
+        'category_id',
+        'name',
+        'brand',
+        'description',
+        'price',
+        'stock',
+        'image_url'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
