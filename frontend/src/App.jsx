bf2bb17374983021fd1d743a9a9f55bfad82956a
Haeryz <haeriz42069@gmail.com>
2024-12-30 15:33:22 +0700

    Footer and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { Box} from "@chakra-ui/react"
-import Navbar from "./components/main/Navbar"
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
+import { Box } from "@chakra-ui/react";
+import Navbar from "./components/main/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Footer from "./components/main/Footer";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Box>
       <Footer />
