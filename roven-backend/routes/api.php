a4ef1aaa36329acf919bb3ca5b115172b58a66f7
andifathir <andi.fathirr@gmail.com>
2024-12-30 15:13:17 +0700

    .

--- roven-backend/routes/api.php
+++ roven-backend/routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\OrderItemController;
+use App\Http\Controllers\Api\ReviewController;
+use App\Http\Controllers\Api\WishlistController;
+use App\Http\Controllers\Api\PaymentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,25 +19,7 @@ Route::resource('products', ProductController::class);
 Route::resource('category', CategoryController::class);
 Route::resource('order', OrderController::class);
 Route::resource('users', UserController::class);
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\OrderItemController;
-use App\Http\Controllers\Api\ReviewController;
-use App\Http\Controllers\Api\WishlistController;
-use App\Http\Controllers\Api\PaymentController;
-
-// Users API Routes
-
-// Order Items API Routes
 Route::apiResource('order-items', OrderItemController::class);
-
-// Reviews API Routes
 Route::apiResource('reviews', ReviewController::class);
-
-// Wishlists API Routes
 Route::apiResource('wishlists', WishlistController::class);
-
-// Payments API Routes
 Route::apiResource('payments', PaymentController::class);
